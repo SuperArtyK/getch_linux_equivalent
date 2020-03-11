29ae304697257092b3ecd7547f69c4c7648f64db
SuperArtyK <61715009+SuperArtyK@users.noreply.github.com>
2020-03-11 10:08:23 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -48,11 +48,10 @@ char getche(void)
 /* Let's test it out */
 int main(void) {
   char c;
-  printf("(getche example) please type a letter: ");
+  for(;;){
+  cout<<"\n(getche example) please type a letter: ";
   c = getche();
-  printf("\nYou typed: %c\n", c);
-  printf("(getch example) please type a letter...");
-  c = getch();
-  printf("\nYou typed: %c\n", c);
+  cout<<"\nYou typed: \n";
+  }  
   return 0;
 }
